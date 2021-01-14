540182a5660c29f0b29cfc687fd128ad04c62489
Drakkkath <elamine.yamani@gmail.com>
2021-01-14 22:16:23 +0100

    Alarm sound added

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from sys import platform
 import tkinter as tk
 from tkinter import *
 import tkinter.font as tkFont
+from playsound import playsound
 
 #Main window of the app
 root = tk.Tk()
@@ -37,6 +38,7 @@ def start_command():
             second = 59
         else:
             start.config(state = ACTIVE)
+            playsound("alarm.mp3")
             return None
     else:
         second -= 1
@@ -47,16 +49,15 @@ def start_command():
 
 
 
+
 minute = 20
 second = 0
 font_style = tkFont.Font(family="Lucida Grande", size=50)
 timer = Label(root, text = "%02d"%minute + ":" + "%02d"%second, font = font_style)
 start = Button(root, text = "Start", command = start_command)
 
-
-timer.pack(pady = 100)
+timer.pack(pady = 50)
 start.pack()
-
 
 
 root.mainloop()
